01634c62dd0c0fcd6e348968e92d85d3ec86e4ef
dylan <825880445@qq.com>
2019-04-30 14:59:22 +0800

    amend

--- src/ProductRow.js
+++ src/ProductRow.js
@@ -2,10 +2,11 @@ import React , {Component} from 'react';
 
 class ProductRow extends Component {
   render(){
+    let item = this.props.item;
     return (
       <tr>
-        <td>{this.props.item.name}</td>
-        <td>{this.props.item.price}</td>
+        <td className={item.stocked?'':'outStocked'}>{item.name}</td>
+        <td>{item.price}</td>
       </tr>
     )
   }
